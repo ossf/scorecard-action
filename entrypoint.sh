2ad993f84c4943cdce2f15ff414e82626dce04d1
laurentsimon <laurentsimon@google.com>
2022-06-06 17:37:04 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,7 @@ set -euo pipefail
 if [[ -z "$INPUT_REPO_TOKEN" ]]; then
     echo "entering"
     # Note: we don't use GITHUB_TOKEN directly because bash complains about "unbound" variable.
-    INPUT_REPO_TOKEN="env | grep GITHUB_TOKEN | cut -d '=' -f2"
+    INPUT_REPO_TOKEN="$(env | grep GITHUB_TOKEN | cut -d '=' -f2)"
     if [[ -z "$INPUT_REPO_TOKEN" ]]; then
         echo "it's empty"
         exit 2
