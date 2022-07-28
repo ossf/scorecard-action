57d7dd075e2dd432c84cf368b06cae6c762032d2
aidenwang <yidingwang9867@gmail.com>
2022-07-27 20:53:40 -0700

    save

--- entrypoint/dependencydiff/helpers.go
+++ entrypoint/dependencydiff/helpers.go
@@ -104,8 +104,6 @@ func entryExists(system, name, version string) (bool, error) {
 	if err != nil {
 		return false, fmt.Errorf("error requesting deps.dec/_: %w", err)
 	}
-	fmt.Println(url)
-	fmt.Println(resp.StatusCode)
 	switch resp.StatusCode {
 	case http.StatusOK:
 		return true, nil
