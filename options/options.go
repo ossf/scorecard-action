c694c355bcd6661db2153dc37269df85b330f59f
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-12-09 12:33:54 -0800

    feat: update logging (#1032)
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    * update
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>
    
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- options/options.go
+++ options/options.go
@@ -137,15 +137,20 @@ func (o *Options) Validate() error {
 
 // Print is a function to print options.
 func (o *Options) Print() {
-	fmt.Printf("Event file: %s\n", o.GithubEventPath)
-	fmt.Printf("Event name: %s\n", o.GithubEventName)
+	// Scorecard options
+	fmt.Println("Scorecard options:")
 	fmt.Printf("Ref: %s\n", o.ScorecardOpts.Commit)
 	fmt.Printf("Repository: %s\n", o.ScorecardOpts.Repo)
+	fmt.Printf("Local: %s\n", o.ScorecardOpts.Local)
+	fmt.Printf("Format: %s\n", o.ScorecardOpts.Format)
+	fmt.Printf("Policy file: %s\n", o.ScorecardOpts.PolicyFile)
+	fmt.Println()
+	fmt.Println("Event / repo information:")
+	fmt.Printf("Event file: %s\n", o.GithubEventPath)
+	fmt.Printf("Event name: %s\n", o.GithubEventName)
 	fmt.Printf("Fork repository: %s\n", o.IsForkStr)
 	fmt.Printf("Private repository: %s\n", o.PrivateRepoStr)
 	fmt.Printf("Publication enabled: %+v\n", o.PublishResults)
-	fmt.Printf("Format: %s\n", o.ScorecardOpts.Format)
-	fmt.Printf("Policy file: %s\n", o.ScorecardOpts.PolicyFile)
 	fmt.Printf("Default branch: %s\n", o.DefaultBranch)
 }
 
