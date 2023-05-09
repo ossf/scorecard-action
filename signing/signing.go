253339ff65614bd1f961eb1671dd65073cf5f715
Bob Callaway <bcallaway@google.com>
2023-05-09 13:22:02 -0700

    bump cosign to v2.0.2 and use v2 golang module
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- signing/signing.go
+++ signing/signing.go
@@ -29,8 +29,8 @@ import (
 	"strings"
 	"time"
 
-	sigOpts "github.com/sigstore/cosign/cmd/cosign/cli/options"
-	"github.com/sigstore/cosign/cmd/cosign/cli/sign"
+	sigOpts "github.com/sigstore/cosign/v2/cmd/cosign/cli/options"
+	"github.com/sigstore/cosign/v2/cmd/cosign/cli/sign"
 
 	"github.com/ossf/scorecard-action/entrypoint"
 	"github.com/ossf/scorecard-action/options"
@@ -62,10 +62,6 @@ func New(token string) (*Signing, error) {
 		return nil, fmt.Errorf("error setting GITHUB_TOKEN env var: %w", err)
 	}
 
-	if err := os.Setenv("COSIGN_EXPERIMENTAL", "true"); err != nil {
-		return nil, fmt.Errorf("error setting COSIGN_EXPERIMENTAL env var: %w", err)
-	}
-
 	return &Signing{
 		token: token,
 	}, nil
@@ -81,12 +77,11 @@ func (s *Signing) SignScorecardResult(scorecardResultsFile string) error {
 		OIDCIssuer:   sigOpts.DefaultOIDCIssuerURL, // OIDC provider to get ID token to auth for Fulcio.
 		OIDCClientID: "sigstore",
 	}
-	regOpts := sigOpts.RegistryOptions{} // Not necessary so we leave blank.
 
 	// This command will use the provided OIDCIssuer to authenticate into Fulcio, which will generate the
 	// signing certificate on the scorecard result. This attestation is then uploaded to the Rekor transparency log.
 	// The output bytes (signature) and certificate are discarded since verification can be done with just the payload.
-	if _, err := sign.SignBlobCmd(rootOpts, keyOpts, regOpts, scorecardResultsFile, true, "", ""); err != nil {
+	if _, err := sign.SignBlobCmd(rootOpts, keyOpts, scorecardResultsFile, true, "", "", true); err != nil {
 		return fmt.Errorf("error signing payload: %w", err)
 	}
 
